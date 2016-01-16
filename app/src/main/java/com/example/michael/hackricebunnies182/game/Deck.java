f0dfe2e1b9de406e522959dbe77b2d77e29e0aae
Michael Peirce <msp700@gmail.com>
2016-01-15 22:09:18 -0600

    Creating new classes

---
+++ app/src/main/java/com/example/michael/hackricebunnies182/game/Deck.java
@@ -0,0 +1,7 @@
+package com.example.michael.hackricebunnies182.game;
+
+/**
+ * Created by Michael on 1/15/2016.
+ */
+public class Deck {
+}
--- app/src/main/java/com/example/michael/hackricebunnies182/game/Player.java
+++ app/src/main/java/com/example/michael/hackricebunnies182/game/Player.java
@@ -1,12 +1,25 @@
 package com.example.michael.hackricebunnies182.game;
 
+import android.graphics.Color;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Michael on 1/15/2016.
  */
 public class Player {
-    private int color;
+    private Color color;
     private List<RouteCard> cards;
     private int score;
+
+    public Player(Color color) {
+        this.score = 0;
+        this.color = color;
+        this.cards = new ArrayList<>();
+    }
+
+    public void addCards(List<RouteCard> cards) {
+        cards.addAll(cards);
+    }
 }
