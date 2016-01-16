5b10851731b23fc7e2e857c7338f95706b2986b7
Derek Peirce <derektom14@gmail.com>
2016-01-15 23:02:42 -0600

    Per Jenna's request, PlayerColor no longer contains any Color object. Also, completely abandoned Java 8.

--- app/src/main/java/com/example/michael/hackricebunnies182/game/Player.java
+++ app/src/main/java/com/example/michael/hackricebunnies182/game/Player.java
@@ -1,25 +1,23 @@
 package com.example.michael.hackricebunnies182.game;
 
-import android.graphics.Color;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Michael on 1/15/2016.
+ * A player of the game
  */
 public class Player {
-    private Color color;
-    private List<RouteCard> cards;
+    private final PlayerColor color;
+    private final List<RouteCard> cards;
     private int score;
 
-    public Player(Color color) {
+    public Player(PlayerColor color) {
         this.score = 0;
         this.color = color;
         this.cards = new ArrayList<>();
     }
 
-    public void addCards(List<RouteCard> cards) {
-        cards.addAll(cards);
+    public void addCards(List<RouteCard> addedCards) {
+        cards.addAll(addedCards);
     }
 }
--- app/src/main/java/com/example/michael/hackricebunnies182/game/PlayerColor.java
+++ app/src/main/java/com/example/michael/hackricebunnies182/game/PlayerColor.java
@@ -6,16 +6,6 @@ import java.awt.Color;
  * Created by Derek on 1/15/2016.
  */
 public enum PlayerColor {
-    RED(Color.RED), BLUE(Color.BLUE), YELLOW(Color.YELLOW), GREEN(Color.GREEN), BLACK(Color.BLACK);
-
-    private final Color color;
-
-    private PlayerColor(Color color) {
-        this.color = color;
-    }
-
-    public Color getColor() {
-        return color;
-    }
+    RED, BLUE, YELLOW, GREEN, BLACK;
 
 }
