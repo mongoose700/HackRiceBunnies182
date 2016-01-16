c7bc74aac167ccd26f1171f693a13d31cc5d7f5e
Michael Peirce <msp700@gmail.com>
2016-01-15 22:34:32 -0600

    Created Draw class

--- app/src/main/java/com/example/michael/hackricebunnies182/game/Draw.java
+++ app/src/main/java/com/example/michael/hackricebunnies182/game/Draw.java
@@ -1,7 +1,20 @@
 package com.example.michael.hackricebunnies182.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Michael on 1/15/2016.
  */
 public class Draw {
+
+    private int mustKeep;
+    private Player player;
+    private List<CheckedRouteCard> cards;
+
+    public Draw(List<CheckedRouteCard> cards, Player player, int mustKeep) {
+        this.cards = cards;
+        this.player = player;
+        this.mustKeep = mustKeep;
+    }
 }
