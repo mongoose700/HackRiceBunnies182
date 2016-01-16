e91f98c6acdc4200e948c979e0ea522270d365e6
Michael Peirce <msp700@gmail.com>
2016-01-15 21:31:17 -0600

    Added simple classes

---
+++ app/src/main/java/com/example/michael/hackricebunnies182/game/Player.java
@@ -0,0 +1,12 @@
+package com.example.michael.hackricebunnies182.game;
+
+import java.util.List;
+
+/**
+ * Created by Michael on 1/15/2016.
+ */
+public class Player {
+    private int color;
+    private List<RouteCard> cards;
+    private int score;
+}
